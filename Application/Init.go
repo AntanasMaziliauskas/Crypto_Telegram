0b0d3853ffe65a1a5c3b9b4f0fb5a83585b430ec
AntanasMaziliauskas <maziliauskasantanas@gmail.com>
2019-01-31 16:19:26 +0200

    C

--- Application/Init.go
+++ Application/Init.go
@@ -1,11 +1,13 @@
 package Application
 
-////BOT Authentication paleidimas
-
 func Init() {
+
+	//BOT Authentication iskvietimas
+
 	//FromFile paleidimas
 	app.CryptoRule, err = app.FromFile()
 
 	////URL ID funkcijos paleidimas
 	r := app.URLID()
+
 }
